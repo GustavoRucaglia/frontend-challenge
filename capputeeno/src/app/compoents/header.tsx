6260063a3afa7c24e6bfac085a33439bf849cd11
GustavoRucaglia <111381127+gustavo-Bozetti@users.noreply.github.com>
2024-02-24 23:16:18 -0300

    feat: create header

---
+++ capputeeno/src/app/compoents/header.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import styled from "styled-components";
+
+import { Saira_Stencil_One } from "next/font/google";
+import { PrimaryInput, PrimaryInputWSearchIcon } from "./primary-input";
+import { CartControl } from "./cart-controll";
+
+const sairaStencil = Saira_Stencil_One({ 
+  weight: ["400"],
+  subsets: ["latin"] });
+  interface HeaderProps {}
+  const TagHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 24px;
+
+  > div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 24px;
+  }
+
+  @media (min-width: ${props => props.theme.desktopBreakpoint}){
+      padding: 20px 160px;
+  }
+`
+
+const Logo = styled.a`
+  color: var(--logo-color);
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 150%;
+  text-decoration: none;
+
+  @media(min-width: ${props => props.theme.tableBreakpoint}){
+      font-size: 24px;
+  }
+
+  @media(min-width: ${props => props.theme.desktopBreakpoint}){
+      font-size: 40px;
+  }
+`
+  
+  const Header = () => {
+    return (
+      <TagHeader>
+        <Logo className={sairaStencil.className}>Capputeno</Logo>
+        <div>
+           <PrimaryInputWSearchIcon/> 
+           <CartControl/>
+        </div>
+      </TagHeader>
+    );
+  };
+  
+  export default Header;
---
+++ capputeeno/src/app/compoents/primary-input.tsx
@@ -0,0 +1,43 @@
+import { styled } from "styled-components";
+import { SearchIcon } from "./search-icon";
+import { InputHTMLAttributes } from "react";
+
+export const PrimaryInput = styled.input`
+    width: 300px;
+    height: 40px;
+    border: none;
+    padding-left: 10px;
+
+    background: var(--bg-secondary);
+
+    border-radius: 8px;
+
+    font-family: inherit;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    color: var(--text-dark);
+    `
+
+const InputContainer = styled.div`
+   position: relative;
+   width: 300px;
+
+   svg{
+       position: absolute;
+       right: 10px;
+       top: 50%;
+       transform: translateY(-50%);
+   }
+`
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{}
+
+export function PrimaryInputWSearchIcon(props: InputProps){
+    return(
+        <InputContainer>
+            <PrimaryInput {...props} />
+            <SearchIcon/>
+        </InputContainer>
+    )
+}
